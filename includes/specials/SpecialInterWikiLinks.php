5234e7a5d8b13ce5ccf4c316f4941e1949cd7e83
Peter Boehm <boehm@hallowelt.com>
2019-03-06 17:20:43 +0100

    Code Cleanup Refactoring for BSP3
    using Extension.php instead of .class.php
    removing code smell. fixed Api.
    
    [3.1]
    
    Change-Id: If0a58a58d8e4297ea2ce4b4a434ff68a90950362

--- includes/specials/SpecialInterWikiLinks.php
+++ includes/specials/SpecialInterWikiLinks.php
@@ -9,23 +9,30 @@
  * @package    BlueSpice_InterWikiLinks
  * @subpackage InterWikiLinks
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  * @filesource
  */
 class SpecialInterWikiLinks extends \BlueSpice\SpecialPage {
 
+	/**
+	 *
+	 */
 	public function __construct() {
 		parent::__construct( 'InterWikiLinks', 'interwikilinks-viewspecialpage' );
 	}
 
+	/**
+	 *
+	 * @param string $par URL parameters to special page.
+	 */
 	public function execute( $par ) {
 		parent::execute( $par );
-		$oOutputPage = $this->getOutput();
+		$outputPage = $this->getOutput();
 
-		$oOutputPage->addModules( 'bluespice.insertLink.interWikiLinks' );
+		$outputPage->addModules( 'bluespice.insertLink.interWikiLinks' );
 
-		$oOutputPage->addModules( 'ext.bluespice.interWikiLinks' );
-		$oOutputPage->addHTML( '<div id="InterWikiLinksGrid" class="bs-manager-container"></div>' );
+		$outputPage->addModules( 'ext.bluespice.interWikiLinks' );
+		$outputPage->addHTML( '<div id="InterWikiLinksGrid" class="bs-manager-container"></div>' );
 	}
 
 }
--- src/AdminTool.php
+++ src/AdminTool.php
@@ -6,35 +6,59 @@ use BlueSpice\IAdminTool;
 
 class AdminTool implements IAdminTool {
 
+	/**
+	 *
+	 * @return string
+	 */
 	public function getURL() {
 		$tool = \SpecialPage::getTitleFor( 'InterWikiLinks' );
 		return $tool->getLocalURL();
 	}
 
+	/**
+	 *
+	 * @return \Message
+	 */
 	public function getDescription() {
 		return wfMessage( 'bs-interwikilinks-desc' );
 	}
 
+	/**
+	 *
+	 * @return \Message
+	 */
 	public function getName() {
 		return wfMessage( 'bs-interwikilinks-label' );
 	}
 
+	/**
+	 *
+	 * @return array
+	 */
 	public function getClasses() {
-		$classes = array(
+		$classes = [
 			'bs-icon-chain'
-		);
+		];
 
 		return $classes;
 	}
 
+	/**
+	 *
+	 * @return array
+	 */
 	public function getDataAttributes() {
 		return [];
 	}
 
+	/**
+	 *
+	 * @return array
+	 */
 	public function getPermissions() {
-		$permissions = array(
+		$permissions = [
 			'interwikilinks-viewspecialpage'
-		);
+		];
 		return $permissions;
 	}
 
