4647240c04ff5d2f2bcaf0962a11aeb050f30ec3
pwirth <wirth@hallowelt.biz>
2021-10-18 13:40:39 +0200

    Seperate userinterface hooks and fix naming
    ERM:23871
    [4.x]
    
    Change-Id: I6ead142c97c5922e3a5788df84c82aeec4a60312

--- src/HookHandler/CommonUserInterface.php
+++ src/HookHandler/CommonUserInterface.php
@@ -5,7 +5,7 @@ namespace BlueSpice\InterWikiLinks\HookHandler;
 use BlueSpice\InterWikiLinks\GlobalActionsManager;
 use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
 
-class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+class CommonUserInterface implements MWStakeCommonUIRegisterSkinSlotComponents {
 
 	/**
 	 * @inheritDoc
